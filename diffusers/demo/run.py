d42a429352e6c46176ebbca90e5e6247a0bf4c2e
Thomas Kragerud <thomas.kragerud@gmail.com>
2023-11-24 16:59:19 -0800

    mmmmhmm

---
+++ diffusers/demo/run.py
@@ -0,0 +1,36 @@
+import os 
+import subprocess
+
+def main():
+
+    # path to training file
+    train_file_path = "../examples/dreambooth/train_dreambooth.py"
+
+    # Set environment variables 
+    os.environ['MODEL_NAME'] = "CompVis/stable-diffusion-v1-4"
+    os.environ['INSTANCE_DIR'] = "dog"
+    os.environ['OUTPUT_DIR'] = "out"
+
+    # Construct the command
+    command = f"""
+    accelerate launch {train_file_path} \
+      --pretrained_model_name_or_path={os.environ['MODEL_NAME']} \
+      --instance_data_dir={os.environ['INSTANCE_DIR']} \
+      --output_dir={os.environ['OUTPUT_DIR']} \
+      --instance_prompt="a photo of sks dog" \
+      --resolution=512 \
+      --train_batch_size=1 \
+      --gradient_accumulation_steps=1 \
+      --learning_rate=5e-6 \
+      --lr_scheduler="constant" \
+      --lr_warmup_steps=0 \
+      --max_train_steps=400 \
+      --push_to_hub
+    """
+
+    # Execute the command
+    subprocess.run(command, shell=True)
+
+if __name__ == "__main__":
+    print("yoooo")
+    main()
