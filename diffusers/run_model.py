212e3fde912c3fbcc639e9462037310ae088baab
Thomas Kragerud <thomas.kragerud@gmail.com>
2023-12-06 22:43:58 -0800

    better inference with image types

--- diffusers/run_model.py
+++ diffusers/run_model.py
@@ -1,5 +1,7 @@
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
+from collections import defaultdict
+
 from matplotlib import pyplot as plt
 
 pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
@@ -7,11 +9,13 @@ pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.to("cuda")
 
 pipe.unet.load_attn_procs("model")
-
+image_type = defaultdict(int)
 i = 0
 while True:
     prompt = input("Input prompt: ")
+    image_type_name = input("give a image type: ").lower()
+    image_type[image_type_name] += 1
     image = pipe(prompt, num_inference_steps=25).images[0]
-    image.save(f"outputimages/output_{i}.png")
-    print(f"Saved output image as outputimages/output_{i}.png")
-    i += 1
+    file_name = f'{image_type_name}_{image_type[image_type_name]}_in:{prompt}'
+    image.save(f"outputimages/{file_name}.png")
+    print(f"Saved output image as outputimages/{file_name}.png")
